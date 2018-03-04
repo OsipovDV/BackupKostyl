7aeddfbc2319c40af0991e1b8574769aa21a1580
OsipovDV <osipov_dv@mail.ru>
2018-03-04 19:10:11 +0300

    Version 1.2

--- sysstate_backup.ps1
+++ sysstate_backup.ps1
@@ -7,20 +7,36 @@ $confFilesToBackup = ""
 $confFilesToExclude = ""
 $confShareUser = "share\user"
 $confSharePwd = "pwd"
+$date = Get-Date -format "dd-MM-yyyy_HH-mm"
+$computername = $env:COMPUTERNAME
+$vhdpath = "$confBackupPath\sysstate-$computername-$date.vhd"
 
 #
 # Code area, don't change anything bellow this line
 #
 
+
+# Map network share
 $password = ConvertTo-SecureString $confSharePwd -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $confShareUser, $password
-
-# used for SMB Share
-New-PSDrive -name BackupDrive -root $confBackupPath -Credential $cred -PSProvider FileSystem
+New-PSDrive -name NetShare -root $confBackupPath -Credential $cred -PSProvider FileSystem
 
 
+# Initialize script for diskpart, to create vhd on network share
+"CREATE VDISK FILE=$vhdpath type=EXPANDABLE MAXIMUM=2000000" | Out-File -FilePath "C:\_scripts\vhd_templ.txt" -Encoding ascii
+"SELECT VDISK FILE=$vhdpath" | Out-File -FilePath "C:\_scripts\vhd_templ.txt" -Encoding ascii -Append
+"ATTACH VDISK" | Out-File -FilePath "C:\_scripts\vhd_templ.txt" -Encoding ascii -Append
+"CREATE PARTITION PRIMARY" | Out-File -FilePath "C:\_scripts\vhd_templ.txt" -Encoding ascii -Append
+"FORMAT FS=NTFS QUICK" | Out-File -FilePath "C:\_scripts\vhd_templ.txt" -Encoding ascii -Append
+"ASSIGN LETTER=X" | Out-File -FilePath "C:\_scripts\vhd_templ.txt" -Encoding ascii -Append
+
+diskpart.exe /s "C:\_scripts\vhd_templ.txt"
+
+
+
+# Initialize backup settings
 $policy = New-WBPolicy
-$backupLocation = New-WBBackupTarget -networkpath $confBackupPath
+$backupLocation = New-WBBackupTarget -VolumePath X:
 
 
 if($confFilesToBackup)
@@ -41,8 +57,6 @@ if($confFilesToExclude)
     }
 }
 
-
-
 if($confBackupSystemState) 
 {
 	Add-WBSystemState -Policy $policy
@@ -50,11 +64,20 @@ if($confBackupSystemState)
 
 Add-WBBackupTarget -Policy $policy -Target $backupLocation
 Set-WBVssBackupOptions -Policy $policy -Vssfull
+
+#
+# Start Backup
+#
 Start-WBBackup -Policy $policy
 
-if ((Get-WBJob -Previous 1).JobState -eq "Completed")
+
+#
+# Backup rotation mechanism
+#
+
+<#if ((Get-WBJob -Previous 1).JobState -eq "Completed" -and  (Get-WBJob -Previous 1).HResult -eq 0)
 {
-    $temp = ($confBackupPath + "\WindowsImageBackup\" + $env:COMPUTERNAME)
+    $temp = ($confBackupPath + "\WindowsImageBackup\" + $computername)
     if((Test-Path -Path ($temp + ".2")) -eq $true)
     {
         Remove-Item -Path ($temp + ".2") -Force -Recurse
@@ -68,7 +91,14 @@ if ((Get-WBJob -Previous 1).JobState -eq "Completed")
     
     Rename-Item -Path ($temp) -NewName ($temp + ".1") ;
   
-}
-#
-Remove-PSDrive -name BackupDrive
+}#>
+
+# Initialize script for diskpart, to unmount vhd
+"SELECT VDISK FILE=$vhdpath" | Out-File -FilePath "C:\_scripts\vhd_templ.txt" -Encoding ascii
+"DETACH VDISK" | Out-File -FilePath "C:\_scripts\vhd_templ.txt" -Encoding ascii -Append
+diskpart.exe /s "C:\_scripts\vhd_templ.txt"
+
+
+# Unmap network share
+Remove-PSDrive -name NetShare
 
